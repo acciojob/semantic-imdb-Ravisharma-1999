3d59b167b487e582ce320c3af6d0420206545d3f
Ravisharma-1999 <ravi829144@gmail.com>
2024-07-05 14:18:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+describe('Movie Detail Page', () => {
+    const baseUrl = 'http://localhost:3000'; // Adjust this to match your local setup
+
+    it('should have a non-empty summary section', () => {
+        cy.visit(baseUrl + "/main.html");
+        cy.get('#summary').should('exist');
+        cy.get('#summary').within(() => {
+            cy.get('p').should('exist').and('not.be.empty');
+        });
+    });
+});
